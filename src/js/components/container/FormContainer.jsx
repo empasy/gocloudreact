0b7d65c1f5b9868529c3a2fee10caa3e5369256d
Gary <garyarcediaz@gmail.com>
2019-06-10 01:35:08 -0400

    Se elimina limpieza de inputs

--- src/js/components/container/FormContainer.jsx
+++ src/js/components/container/FormContainer.jsx
@@ -88,14 +88,6 @@ class FormContainer extends Component {
 
             let data = await response.json();
             this.setState({
-                nombre: "",
-                nombreValidate: false,
-                telefono: "",
-                telefonoValidate: false,
-                correo: "",
-                correoValidate: false,
-                rut: "",
-                rutValidate: false,
                 alerta: {
                     show: true,
                     msj: data.msj,
@@ -104,14 +96,6 @@ class FormContainer extends Component {
             });
         } else {
             this.setState({
-                nombre: "",
-                nombreValidate: false,
-                telefono: "",
-                telefonoValidate: false,
-                correo: "",
-                correoValidate: false,
-                rut: "",
-                rutValidate: false,
                 alerta: {
                     show: true,
                     msj: 'Existen campos que no cumple con lo requerido, favor seguir los Ej.',
